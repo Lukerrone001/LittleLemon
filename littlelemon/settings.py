4e8beb1c03ecb8c20b8ff20e2978e7cf075fd616
Lukerrone <perroneluca2001@gmail.com>
2024-10-20 15:42:31 +0200

    Postgres no more Mysql!

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'LittleLemon',
-        'USER': 'root',
+        'USER': 'postgres',
         'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'HOST': 'localhost',
+        'PORT': '5432',
         #'OPTIONS': { 
             #'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class menu(models.Model):
+    pass
+
+class booking(models.Model):
+    pass
